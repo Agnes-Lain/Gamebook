0925ee711e23d7f2527aa5ace1843d588f0cdf82
Yizhu ZONG <agnes.lain@gmail.com>
2020-11-17 01:35:16 +0100

    add search bar, color set, fixed navbar, dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,18 @@
-require 'json'
-
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home ]
+
+  NOWDATE = Time.now.to_date.to_s
+  LASTDATE = (NOWDATE.to_date - 183).to_s
+  URL = "https://api.rawg.io/api/games?dates=#{LASTDATE},#{NOWDATE}&ordering=-added"
 
   def home
-    @responses = HTTParty.get('https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-added')["results"][0..11]
+    @responses = HTTParty.get(URL)["results"][0..11]
   end
 
   def dashboard
+    @responses = HTTParty.get(URL)["results"][0..11]
     @user = current_user
     @consoles = @user.consoles
-    @games = @user.games
+    @user_games = @user.games
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'dashboard', to: 'pages#dashboard'
+  resources :games, only:[ :new, :create ]
 
 end
