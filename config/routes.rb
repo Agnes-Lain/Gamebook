13bc117e4aebd5d8522a23cc726bf93ad9b6fe76
Yizhu ZONG <agnes.lain@gmail.com>
2020-11-18 23:47:54 +0100

    small changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'dashboard', to: 'pages#dashboard'
-  resources :games, only:[ :new, :create ]
+  resources :games, only:[ :create ]
 
 end
